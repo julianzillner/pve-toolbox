300d5b17a0c8ca26c4250427a9086594787f79ef
Julian Zillner <80313417+julianzillner@users.noreply.github.com>
2022-04-06 20:52:32 +0200

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,1 +1,12 @@
-mkdir hello
+read -p "Enter Directory Name: " dirname
+if [[ ! -d "$dirname" ]]
+then
+        if [[ ! -L $dirname ]]
+        then
+                echo "Directory doesn't exist. Creating now"
+                mkdir $dirname
+                echo "Directory created"
+        else
+                echo "Directory exists"
+        fi
+fi
